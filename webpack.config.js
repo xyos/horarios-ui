025e9a92bbe4ac7da208d0aa302fc45ea0dfd3bb
Jairo Suarez <jasuarezpr@gmail.com>
2015-01-10 17:56:00 -0500

    css separated from bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context : __dirname,
   entry : [
-    'webpack/hot/dev-server',
     './app/components/Application'
   ],
   output : {
@@ -19,8 +19,16 @@ module.exports = {
       {test: /\.jsx$/, loaders: ['react-hot', 'jsx-loader?harmony&insertPragma=React.DOM']},
       {test: /\.es6\.js$/, loader: 'es6-loader'},
       // Extract css files
-      { test: /\.css$/,    loader: "style-loader!css-loader" },
-      { test: /\.less$/,   loader: "style-loader!css-loader!less-loader" },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      // Optionally extract less files
+      // or any other compile-to-css language
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      },
       // allow less files to load urls pointing to font assets
       // @TODO: figure out why this is necessary and do it better
       {test: /\.(woff|ttf|eot|svg|png)$/, loader: 'file-loader' }
@@ -39,7 +47,8 @@ module.exports = {
       $: "jquery",
       jQuery: "jquery",
       "window.jQuery": "jquery"
-    })
+    }),
+    new ExtractTextPlugin("[name].css")
   ]
 
 
